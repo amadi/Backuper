2c52ab6eeaed5969519f15ead57f6eec1f51fd86
amadi <dimanfet@gmail.com>
2012-07-24 17:07:18 +0400

    Получение списка каталогов с сервера

--- backuper.rb
+++ backuper.rb
@@ -1,25 +1,62 @@
 require 'net/ftp'
+require 'yaml'
 
-#print "FTP: "
-addr=gets
+class Backuper
+	attr_accessor :files, :folders
+	def initialize(addr, login, pw, root_dir)
+		@addr, @login, @pw, @root_dir = addr, login, pw, root_dir
+		@folders = Array.new
+	end
 
-#print "Login: "
-login=gets
+	def ls
+		@files = @ftp.ls("--almost-all")
+		@files
+	end
 
-#print "Password: "
-pw=sets
+	def listFolders(dir = @root_dir)
+		@ftp.chdir(dir)
+		@regexp = %r{(?<isDir>.{1}).+\d{2}:\d{2}\s(?<name>.+)}x
+		@ftp.ls("-al").each do |item|
+			r = @regexp.match(item)
+			#puts "isDir: #{r[:isDir]}, name: #{r[:name]}"
+			if (r[:isDir] == "d") and (r[:name]!='.' and r[:name]!='..')
+				@folders.push("#{dir}/#{r[:name]}")
+			end
+		end
+		#puts @folders
+	end
 
-ftp = Net::FTP.new(addr)
-ftp.login(login,pw)
+	def connect
+		puts "\e[30;47mConnecting to #{@addr} as #{@login}\e[0m"
+		@ftp = Net::FTP.new(@addr)
+		@ftp.login(@login,@pw)
+		@ftp.chdir(@root_dir)
+		puts "\e[32mSuccess!\e[0m"
+	end
 
-files = ftp.nlst
-
-
-puts "keys of hash \"files\": "
-for key in 0...files.length
-	print "key  : ", files[key], "\n"
+	def	close
+		@ftp.close
+		puts "\e[30;47mConnection closed\e[0m"
+	end
 end
-puts "List of files in current dir:"
-puts files
 
-ftp.close
+
+YAML::ENGINE.yamler = 'syck'
+servers = begin
+	YAML.load_file('ftp.yml')
+rescue ArgumentError => e
+	puts "Could not parse ftp.yml file. #{e.message}"
+end
+
+servers.each do |server, field|
+	ftp = Backuper.new(field["host"], field["login"], field["pw"], field["root_dir"])
+	ftp.connect
+
+	ftp.listFolders
+
+	ftp.folders.each {|folder| ftp.listFolders(folder)}
+	puts ftp.folders
+
+	ftp.close
+end
+
