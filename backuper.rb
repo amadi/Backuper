99a016b790c5cd7116ea8f72be4c4740b772c1b9
amadi <dimanfet@gmail.com>
2012-07-25 12:48:12 +0400

    Версия 0.1 Есть список ftp для бэкапа. Есть отображение процесса бэкапа. Есть результат бэкапа =)

--- backuper.rb
+++ backuper.rb
@@ -1,10 +1,11 @@
 require 'net/ftp'
 require 'yaml'
+require 'fileutils'
 
 class Backuper
-	attr_accessor :files, :folders, :addr, :size
-	def initialize(addr, login, pw, root_dir)
-		@addr, @login, @pw, @root_dir = addr, login, pw, root_dir
+	attr_accessor :files, :folders, :addr, :size, :server
+	def initialize(addr, login, pw, root_dir, backup_dir, server)
+		@addr, @login, @pw, @root_dir, @backup_dir, @server = addr, login, pw, root_dir, backup_dir, server
 		@folders = Array.new
 		@files = Hash.new
 		@size = 0
@@ -12,7 +13,7 @@ class Backuper
 
 	def listFolders(dir = @root_dir)
 		@ftp.chdir(dir)
-		@regexp = %r{(?<isDir>.{1})(?<mode>.{9})\s+(?<links>\d+)\s+(?<owner>\w+)\s+(?<group>\w+)\s+(?<size>\d+)\s+(?<modifed>\w+\s+\d+\s+\d{2}:\d{2})\s(?<name>.+)}x
+		@regexp = %r{(?<isDir>.{1})(?<mode>.{9})\s+(?<links>\d+)\s+(?<owner>\w+)\s+(?<group>\w+)\s+(?<size>\d+)\s+(?<modifed>\w+\s+\d+\s+.+)\s(?<name>.+)}x
 		@ftp.ls("-al").each do |item|
 			r = @regexp.match(item)
 			if (r[:isDir] == "d") and (r[:name]!='.' and r[:name]!='..')
@@ -24,7 +25,30 @@ class Backuper
 				@files.merge!(file)
 			end
 		end
+	end
 
+	def backup
+		time = Time.new
+		dir = "#{@backup_dir}/#{@server}/#{time.day}_#{time.month}_#{time.year}"
+		FileUtils.mkdir_p(dir)
+		FileUtils.mkdir_p("#{dir}#{@root_dir}")
+		@folders.each do |item|
+			FileUtils.mkdir("#{dir}#{item}")
+		end
+		downloaded_summary = 0
+		i=0
+		@files.each do |key, value|
+			downloaded = 0
+			i=i+1
+			@ftp.getbinaryfile( key, "#{dir}#{key}", 128) do |blk|
+  				downloaded = downloaded + 128
+  				printf("\rfile #{i}(#{(value['size'].to_i) / 1024}Kb) of #{@files.length}.  Downloaded: #{(downloaded_summary) / 1024} Kb     ")
+    			$stdout.flush
+  			end
+  			downloaded_summary = downloaded_summary+value['size'].to_i
+		end
+		puts ''
+		puts "#{@server} backup complete!"
 	end
 
 	def connect
@@ -51,7 +75,7 @@ rescue ArgumentError => e
 end
 
 servers.each do |server, field|
-	ftp = Backuper.new(field["host"], field["login"], field["pw"], field["root_dir"])
+	ftp = Backuper.new(field["host"], field["login"], field["pw"], field["root_dir"], field['backup_dir'], server)
 	ftp.connect
 
 	#get dirs of ftp
@@ -64,6 +88,7 @@ servers.each do |server, field|
 	size = 0
 	puts "Files count: #{ftp.files.length}, total size: #{ftp.size/1024} Kb"
 
+	ftp.backup
 
 	ftp.close
 end
