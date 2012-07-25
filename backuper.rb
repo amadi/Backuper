f6acff495528f3360dad1a1d4ab60e4f0d79e979
amadi <dimanfet@gmail.com>
2012-07-25 10:34:47 +0400

    Обновление. Теперь бэкапер умеет получать список папок и файлов.

--- backuper.rb
+++ backuper.rb
@@ -2,33 +2,35 @@ require 'net/ftp'
 require 'yaml'
 
 class Backuper
-	attr_accessor :files, :folders
+	attr_accessor :files, :folders, :addr, :size
 	def initialize(addr, login, pw, root_dir)
 		@addr, @login, @pw, @root_dir = addr, login, pw, root_dir
 		@folders = Array.new
-	end
-
-	def ls
-		@files = @ftp.ls("--almost-all")
-		@files
+		@files = Hash.new
+		@size = 0
 	end
 
 	def listFolders(dir = @root_dir)
 		@ftp.chdir(dir)
-		@regexp = %r{(?<isDir>.{1}).+\d{2}:\d{2}\s(?<name>.+)}x
+		@regexp = %r{(?<isDir>.{1})(?<mode>.{9})\s+(?<links>\d+)\s+(?<owner>\w+)\s+(?<group>\w+)\s+(?<size>\d+)\s+(?<modifed>\w+\s+\d+\s+\d{2}:\d{2})\s(?<name>.+)}x
 		@ftp.ls("-al").each do |item|
 			r = @regexp.match(item)
-			#puts "isDir: #{r[:isDir]}, name: #{r[:name]}"
 			if (r[:isDir] == "d") and (r[:name]!='.' and r[:name]!='..')
 				@folders.push("#{dir}/#{r[:name]}")
 			end
+			if r[:isDir] != "d"
+				@size = @size + r[:size].to_i
+				file = { "#{dir}/#{r[:name]}" => { "filename" => r[:name], "size" => r[:size], "modifed" => r[:modifed] } }
+				@files.merge!(file)
+			end
 		end
-		#puts @folders
+
 	end
 
 	def connect
 		puts "\e[30;47mConnecting to #{@addr} as #{@login}\e[0m"
 		@ftp = Net::FTP.new(@addr)
+		@ftp.passive = true
 		@ftp.login(@login,@pw)
 		@ftp.chdir(@root_dir)
 		puts "\e[32mSuccess!\e[0m"
@@ -52,11 +54,16 @@ servers.each do |server, field|
 	ftp = Backuper.new(field["host"], field["login"], field["pw"], field["root_dir"])
 	ftp.connect
 
+	#get dirs of ftp
 	ftp.listFolders
-
+	puts "Request directories of #{ftp.addr}"
 	ftp.folders.each {|folder| ftp.listFolders(folder)}
-	puts ftp.folders
+	puts "Count of folders: #{ftp.folders.length}"
+
+	#get files
+	size = 0
+	puts "Files count: #{ftp.files.length}, total size: #{ftp.size/1024} Kb"
+
 
 	ftp.close
 end
-
